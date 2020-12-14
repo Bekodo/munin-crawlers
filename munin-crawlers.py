c901c5804f5a5390619616d730a1497387c4b215
Hector Solans <hectors@gmail.com>
2020-12-14 14:27:30 +0100

    Update munin-crawlers.py
    Add total Traffic line

--- munin-crawlers.py
+++ munin-crawlers.py
@@ -7,27 +7,25 @@ class Monitor(object):
     file_name = ''
     periods = []
     useragents = {}
-    limitloglines = 25000
-    #Strings to get Crawlers un User-Agent, you can add more strings
+    limitloglines = 2500
     botstrings = ['bot', 'AMPHTML']
-    #List of User-Agents defined with the strings of botstrings, you can add more User-Agents
     listuseragents = ['Googlebot', 'AhrefsBot', 'SemrushBot', 'bingbot', 'aspiegel', 'Applebot', 'AMPHTML',  'mj12bot', 'Twitterbot']
 
     def __init__(self):
         self.file_name = os.environ.get('file_name')
-        self.botstrings = [x.lower() for x in self.botstrings]
         for i in range(1,6):
             time = datetime.datetime.now() - datetime.timedelta(minutes=i)
             self.periods.append(time.strftime("[%d/%b/%Y:%H:%M:"))
-
+            
     def __classifie_user_agent(self,useragent):
         for value in self.listuseragents:
-            if value.lower() in useragent.lower(): 
+            if value.lower() in useragent.lower():
                 return value
 
     def __check_if_string_in_file(self):
-        useragents_desordered = {'others': 0}
+        useragents_desordered = {'others': 0, 'Total': 0}
         counter = 0
+
         with open(self.file_name, 'r') as read_obj:
             for line in (read_obj.readlines() [-self.limitloglines:]):
                 if any(time in line for time in self.periods):
@@ -37,6 +35,7 @@ class Monitor(object):
                     line = line.replace("\\","")
                     try:
                         data_json = json.loads(line)
+                        useragents_desordered['Total'] +=1
                         if any(useragent in data_json['userAgent'].lower() for useragent in self.botstrings):
                             UserAgent = self.__classifie_user_agent(data_json['userAgent'])
                             if (UserAgent):
@@ -48,8 +47,6 @@ class Monitor(object):
                                 useragents_desordered['others'] +=1
                     except:
                         pass
-        if not 'others' in useragents_desordered:
-            useragents_desordered['others']=0
         self.useragents = sorted(useragents_desordered.items(), key=lambda x: x[1], reverse=True)
 
     def printValue(self):
@@ -61,16 +58,16 @@ class Monitor(object):
     def __setconfOrder(self):
         order = "graph_order others"
         for item in self.useragents:
-            if (str(item[0]) != 'others'):
+            if (str(item[0]) != 'others' and str(item[0]) != 'Total'):
                 order += " " + str(item[0])
         order += "\n"
         return order
 
     def printConf(self):
         self.__check_if_string_in_file()
-        config = "graph_title " + " Crawlers\n"\
+        config = "graph_title " + " Total Request\n"\
         "graph_args --base 1000 -r --lower-limit 0\n"\
-        "graph_vlabel number of Crawlers Request\n"\
+        "graph_vlabel number of Request\n"\
         "graph_period second\n"\
         "graph_category " + "system\n"
         config += self.__setconfOrder()
@@ -78,8 +75,11 @@ class Monitor(object):
             config += item[0]+".label " + item[0] + "\n"
             if (item[0] == 'others'):
                 config += item[0]+".draw " + "AREA" + "\n"
+            elif (item[0] == 'Total'):
+                config += item[0]+".draw " + "LINE1" + "\n"
+                config += item[0]+".colour " + "454545" + "\n"
             else:
-                config += item[0]+".draw " + "STACK" + "\n"
+               config += item[0]+".draw " + "STACK" + "\n"
             config += item[0]+".type " + "GAUGE" + "\n"
         return config[:-1]
 
