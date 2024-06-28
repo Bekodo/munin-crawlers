d1e2c0e15a0be186aadce6f2e233c291e4903f90
Hector Solans <hectors@gmail.com>
2024-06-28 14:43:03 +0200

    Update Logical and add more Crawlers

--- munin-crawlers.py
+++ munin-crawlers.py
@@ -1,87 +1,118 @@
 #!/usr/bin/python3
 
-import json, sys, os
+import json
+import sys
+import os
 import datetime
 
-class Monitor(object):
-    file_name = ''
-    periods = []
-    useragents = {}
-    limitloglines = 2500
-    botstrings = ['bot', 'AMPHTML']
-    listuseragents = ['Googlebot', 'AhrefsBot', 'SemrushBot', 'bingbot', 'aspiegel', 'Applebot', 'AMPHTML',  'mj12bot', 'Twitterbot']
-
+class Monitor:
     def __init__(self):
         self.file_name = os.environ.get('file_name')
+        self.periods = []
+        self.useragents = {}
+        self.limitloglines = 5000
+        self.listuseragents = [
+            'Googlebot',
+            'AhrefsBot',
+            'SemrushBot',
+            'bingbot',
+            'aspiegel',
+            'Applebot',
+            'AMPHTML',
+            'mj12bot',
+            'Twitterbot',
+            'BLEXBot',
+            'DotBot',
+            'GPTBot',
+            'Amazonbot',
+            'Siteimprove',
+            'SeekportBot',
+            'yandex',
+            'ClaudeBot',
+            'facebookexternalhit',
+            'UptimeRobot',
+            'GoogleOther',
+            'AwarioBot'
+        ]
+        self.botstrings = ['bot', 'craw']
+        
         for i in range(1,6):
             time = datetime.datetime.now() - datetime.timedelta(minutes=i)
             self.periods.append(time.strftime("[%d/%b/%Y:%H:%M:"))
-            
-    def __classifie_user_agent(self,useragent):
+
+    def __classify_user_agent(self, useragent):
         for value in self.listuseragents:
             if value.lower() in useragent.lower():
                 return value
+        for bot in self.botstrings:
+            if bot in useragent:
+                return 'others'
+        return None
 
     def __check_if_string_in_file(self):
-        useragents_desordered = {'others': 0, 'Total': 0}
-        counter = 0
+        useragents_count = {'others': 0, 'Total': 0}
 
-        with open(self.file_name, 'r') as read_obj:
-            for line in (read_obj.readlines() [-self.limitloglines:]):
-                if any(time in line for time in self.periods):
-                    line = line.replace("\r","")
-                    line = line.replace("\t","")
-                    line = line.replace("\n","")
-                    line = line.replace("\\","")
-                    try:
-                        data_json = json.loads(line)
-                        useragents_desordered['Total'] +=1
-                        if any(useragent in data_json['userAgent'].lower() for useragent in self.botstrings):
-                            UserAgent = self.__classifie_user_agent(data_json['userAgent'])
-                            if (UserAgent):
-                                if UserAgent in useragents_desordered:
-                                    useragents_desordered[UserAgent] += 1
-                                else:
-                                    useragents_desordered[UserAgent] = 1
-                            else:
-                                useragents_desordered['others'] +=1
-                    except:
-                        pass
-        self.useragents = sorted(useragents_desordered.items(), key=lambda x: x[1], reverse=True)
+        try:
+            with open(self.file_name, 'r') as read_obj:
+                # Leer las últimas limitloglines líneas del archivo
+                lines = read_obj.readlines()[-self.limitloglines:]
+        except FileNotFoundError:
+            print(f"File {self.file_name} not found.")
+            return
+        except Exception as e:
+            print(f"Error reading file {self.file_name}: {e}")
+            return
+
+        for line in lines:
+            if any(time in line for time in self.periods):
+                try:
+                    data_json = json.loads(line)
+                    useragents_count['Total'] += 1
+                    user_agent = data_json.get('userAgent', '').lower()
+                    classified_user_agent = self.__classify_user_agent(user_agent)
+                    if classified_user_agent:
+                        useragents_count[classified_user_agent] = useragents_count.get(classified_user_agent, 0) + 1
+                except json.JSONDecodeError:
+                    continue
+
+        self.useragents = sorted(useragents_count.items(), key=lambda x: x[1], reverse=True)
 
     def printValue(self):
         self.__check_if_string_in_file()
-        for key in self.useragents:
-            if key:
-                print("{}.value {}".format(key[0],key[1]))
+        for key, value in self.useragents:
+            print(f"{key}.value {value}")
 
     def __setconfOrder(self):
         order = "graph_order others"
         for item in self.useragents:
-            if (str(item[0]) != 'others' and str(item[0]) != 'Total'):
-                order += " " + str(item[0])
+            if item[0] not in ('others', 'Total'):
+                order += f" {item[0]}"
         order += "\n"
         return order
 
     def printConf(self):
         self.__check_if_string_in_file()
-        config = "graph_title " + " Total Request\n"\
-        "graph_args --base 1000 -r --lower-limit 0\n"\
-        "graph_vlabel number of Request\n"\
-        "graph_period second\n"\
-        "graph_category " + "system\n"
+        config = (
+            "graph_title Total Request\n"
+            "graph_args --base 1000 -r --lower-limit 0\n"
+            "graph_vlabel number of Request\n"
+            "graph_period second\n"
+            "graph_category system\n"
+        )
         config += self.__setconfOrder()
         for item in self.useragents:
-            config += item[0]+".label " + item[0] + "\n"
-            if (item[0] == 'others'):
-                config += item[0]+".draw " + "AREA" + "\n"
-            elif (item[0] == 'Total'):
-                config += item[0]+".draw " + "LINE1" + "\n"
-                config += item[0]+".colour " + "454545" + "\n"
+            config += f"{item[0]}.label {item[0]}\n"
+            if item[0] == 'others':
+                config += f"{item[0]}.draw AREA\n"
+            elif item[0] == 'Total':
+                config += (
+                    f"{item[0]}.draw LINE1\n"
+                    f"{item[0]}.colour 454545\n"
+                )
             else:
-               config += item[0]+".draw " + "STACK" + "\n"
-            config += item[0]+".type " + "GAUGE" + "\n"
-        return config[:-1]
+                config += f"{item[0]}.draw STACK\n"
+            config += f"{item[0]}.type GAUGE\n"
+        return config.strip()
 
 if __name__ == '__main__':
     UserAgents = Monitor()
